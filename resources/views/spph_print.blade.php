9f7ecc75114a1ddb30f32804b4f1c9c7aae568bd
gorengan11 <rehansmart2003@gmail.com>
2023-09-07 12:44:08 +0700

    spph

---
+++ resources/views/spph_print.blade.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Template Surat</title>
+    <style type="text/css">
+        @page {
+            size: A4 portrait;
+            margin: 2cm;
+        }
+
+        body {
+            font-family: Arial;
+            font-size: 14px;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            width: 100%;
+            max-width: 700px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .header {
+            margin-bottom: 20px;
+        }
+
+        .logo {
+            width: 200px;
+            height: auto;
+            margin-right: 20px;
+            float: left;
+        }
+
+        .header h2 {
+            font-size: 24px;
+            margin: 0;
+        }
+
+        .header p {
+            font-size: 14px;
+            margin: 5px 0;
+        }
+
+        .line {
+            border-top: 3px solid #000;
+            margin: 10px 0;
+        }
+
+        .address {
+            float: left;
+        }
+
+        .address p {
+            margin: 0;
+        }
+
+        .date {
+            float: right;
+            text-align: right;
+        }
+
+        .info-surat {
+            clear: both;
+            text-align: left;
+            margin-top: 20px;
+        }
+
+        .info-surat p {
+            margin: 5px 0;
+        }
+
+
+        .judul-konten {
+            text-align: center;
+            font-size: 16px;
+            font-weight: bold;
+            margin-top: 20px;
+        }
+
+        .content {
+            margin-top: 20px;
+            line-height: 1.5;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th,
+        td {
+            border: 1px solid #000;
+            padding: 8px;
+            text-align: left;
+        }
+
+        .signature {
+            font-weight: bold;
+            margin-left: 60%;
+            text-align: left;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <img src="https://inkamultisolusi.co.id/api_cms/public/uploads/editor/20220511071342_LSnL6WiOy67Xd9mKGDaG.png" alt="Logo IMSS" class="logo">
+            <h2>PT INKA MULTI SOLUSI SERVICE</h2>
+            <b><p>SERVICE - MAINTENANCE - LOGISTICS - GENERAL CONTRACTOR</p></b>
+            <p>Jl. Salak No. 59 Madiun - 63131</p>
+        </div>
+        <div class="line"></div>
+        <div class="date">
+            <p>Tanggal: 01 September 2023</p>
+        </div>
+        <div class="info-surat">
+            <p><span class="label">Nomor Surat:</span> 001/2023</p>
+            <p><span class="label">Lampiran:</span> 2</p>
+            <p><span class="label">Perihal:</span> Contoh Surat</p>
+        </div>
+        <div class="address">
+            <br><br>
+            <p>Kepada Yth,</p>
+            <b><p>Nama Penerima</p></b>
+            <p>Alamat Penerima</p>
+        </div>
+        <div style="clear: both;"></div>
+        <div class="judul-konten"><u>SURAT PERMINTAAN PENAWARAN HARGA</u><br>(SPPH)</div>
+        <div class="content">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget est ut lectus iaculis
+                posuere a eget enim. Curabitur at dolor vel quam fermentum efficitur. Sed vel fermentum lorem.
+                Vestibulum ac sapien ac tortor euismod eleifend ac vitae est. Nulla facilisi. Fusce a ligula
+                non ligula euismod elementum. Fusce feugiat, velit vel dictum dapibus, urna urna cursus ante,
+                ac tristique metus lorem vel orci.
+            </p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nama Barang</th>
+                        <th>Jumlah</th>
+                        <th>Harga</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Barang 1</td>
+                        <td>10</td>
+                        <td>$100</td>
+                    </tr>
+                    <tr>
+                        <td>Barang 2</td>
+                        <td>5</td>
+                        <td>$50</td>
+                    </tr>
+                    <tr>
+                        <td>Barang 3</td>
+                        <td>3</td>
+                        <td>$30</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p>
+                Integer rhoncus ex nec tellus iaculis scelerisque. Etiam eu neque sed odio semper scelerisque.
+                Quisque nec arcu in arcu vulputate eleifend a ac felis. Sed vel turpis odio. Nulla facilisi.
+                Aliquam a enim ut lorem tincidunt elementum.
+            </p>
+            <p>Demikian kami sampaikan, atas kerjasamanya diucapkan terima kasih.</p>
+        </div>
+        <div class="signature">
+            <br><br>
+            <p>PT INKA MULTI SOLUSI SERVICE</p>
+            <p>KEPALA DEPARTEMEN LOGISTIK</p>
+            <br><br><br><br>
+            <p>(RUDI HARIYANTO)</p>
+        </div>
+    </div>
+</body>
+
+</html>
