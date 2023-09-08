ca34c34b9ed18c49a9f3361b9fa5b2fa2955fe59
ncip48 <mbahcip00@gmail.com>
2023-09-08 14:11:37 +0700

    pdf page

--- resources/views/spph_print.blade.php
+++ resources/views/spph_print.blade.php
@@ -133,7 +133,9 @@
         </div>
         <div class="info-surat">
             <p><span class="label">Nomor Surat &nbsp;&nbsp;: {{ $spph->nomor_spph }}</span></p>
-            <p><span class="label">Lampiran&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: </span></p>
+            <p><span class="label">Lampiran&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                    {{ __($page_count) }} Lembar</span>
+            </p>
             <p><span class="label">Perihal &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
                 </span>{{ $spph->perihal }}</p>
         </div>
@@ -227,6 +229,12 @@
             </table>
         </div>
     </div>
+    {{-- <script type="text/php">
+        if ( isset($pdf) ) {
+            $font = Font_Metrics::get_font("helvetica", "bold");
+            $pdf->page_text(72, 18, "Header: {PAGE_NUM} of {PAGE_COUNT}", $font, 6, array(0,0,0));
+        }
+    </script> --}}
 </body>
 
 </html>
