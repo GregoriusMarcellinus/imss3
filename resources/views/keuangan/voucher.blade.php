a6523f9d9e8a8728715179c18ccc9e51bbfc114d
gorengan11 <rehansmart2003@gmail.com>
2023-10-26 16:21:02 +0700

    vocer

--- resources/views/keuangan/voucher.blade.php
+++ resources/views/keuangan/voucher.blade.php
@@ -10,10 +10,13 @@
         }
 
         .receipt {
-            width: 300px;
+            /* width: 210mm; Lebar A5 dalam mode landscape
+            height: 148mm; Tinggi A5 dalam mode landscape */
             margin: 0 auto;
             border: 1px solid #000;
             padding: 20px;
+            /* transform: rotate(-90deg); /* Untuk mode landscape
+            transform-origin: left top; Atur rotasi dari kiri atas */
         }
 
         .company-logo {
@@ -72,7 +75,43 @@
 </head>
 <body>
     <div class="receipt">
-        <div class="company-logo"></div>
+        <table width="100%">
+            <tr width="100%">
+                <td align="left" style="width: 25%;">
+                    <img src="https://inkamultisolusi.co.id/api_cms/public/uploads/editor/20220511071342_LSnL6WiOy67Xd9mKGDaG.png"
+                        alt="Logo" width="150" class="logo" /><br>
+                </td>
+                <td align="center" style="width: 50%;">
+                    <div class="company-logo"></div>
+                    <div class="header">
+                    <h2>BUKTI KAS MASUK</h2>
+                    <p>Ingoing Payment Voucher</p>
+                    </div>      
+                </td>
+                <td><div class="checklist">
+                    <label for="bukti-tf">Bukti TF</label>
+                    <input type="checkbox" id="bukti-tf" name="bukti-tf">
+                    <label for="scan">Scan</label>
+                    <input type="checkbox" id="scan" name="scan">
+                </div>
+                </td>
+            </tr>
+            <tr>
+                <td align="left" style="width: 25%;">
+                    <br><br>
+                    Nama Bank : <span></span><br>
+                    Pembayaran : <span></span><br>
+                    Jumlah : <span></span><br>
+                </td>
+
+                <td align="center">
+                    <br><br>
+                    No Ref : <span></span><br>
+                    Tangggal Posting : <span></span><br>
+                    Tanggal Transaksi : <span></span><br>
+                </td>
+            </tr>
+        {{-- <div class="company-logo"></div>
         <div class="header">
             <h2>BUKTI KAS MASUK</h2>
             <p>Ingoing Payment Voucher</p>
@@ -90,7 +129,7 @@
             <p>Nomor Ref: 123456789</p>
             <p>Tanggal Posting: 2023-10-26</p>
             <p>Tanggal Transaksi: 2023-10-25</p>
-        </div>
+        </div> --}}
         <table>
             <tr>
                 <th>Kode</th>
