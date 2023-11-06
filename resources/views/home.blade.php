d7ed9c774b3d5233900caa2764c92cd15b38bd3d
ncip48 <mbahcip00@gmail.com>
2023-11-06 14:27:53 +0700

    justifikasi crud done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -99,35 +99,35 @@
                         </a>
                     </div>
                 @endif
-                @if (Auth::user()->role == 0 || Auth::user()->role == 1 ||  Auth::user()->role == 5)
-                <div class="col-lg-3 col-6">
-                    <a href="{{ route('purchase_order.index') }}">
-                        <div class="small-box bg-primary">
-                            <div class="inner">
-                                <p>Purchase</p>
-                                <h3>Order</h3>
+                @if (Auth::user()->role == 0 || Auth::user()->role == 1 || Auth::user()->role == 5)
+                    <div class="col-lg-3 col-6">
+                        <a href="{{ route('purchase_order.index') }}">
+                            <div class="small-box bg-primary">
+                                <div class="inner">
+                                    <p>Purchase</p>
+                                    <h3>Order</h3>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-hand-holding-usd"></i>
+                                </div>
                             </div>
-                            <div class="icon">
-                                <i class="fas fa-hand-holding-usd"></i>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
                 @endif
-                @if (Auth::user()->role == 0 || Auth::user()->role == 1 || Auth::user()->role == 2|| Auth::user()->role == 3)
-                <div class="col-lg-3 col-6">
-                    <a href="{{ route('purchase_request.index') }}">
-                        <div class="small-box bg-primary">
-                            <div class="inner" style="background-color: coral">
-                                <p>Purchase</p>
-                                <h3>Request</h3>
+                @if (Auth::user()->role == 0 || Auth::user()->role == 1 || Auth::user()->role == 2 || Auth::user()->role == 3)
+                    <div class="col-lg-3 col-6">
+                        <a href="{{ route('purchase_request.index') }}">
+                            <div class="small-box bg-primary">
+                                <div class="inner" style="background-color: coral">
+                                    <p>Purchase</p>
+                                    <h3>Request</h3>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-cart-arrow-down"></i>
+                                </div>
                             </div>
-                            <div class="icon">
-                                <i class="fas fa-cart-arrow-down"></i>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
                 @endif
                 @if (Auth::user()->role == 0 || Auth::user()->role == 1)
                     <div class="col-lg-3 col-6">
@@ -144,48 +144,63 @@
                         </a>
                     </div>
                 @endif
-                @if ( Auth::user()->role == 2 || Auth::user()->role == 3)
-                <div class="col-lg-3 col-6">
-                    <a href="{{ route('product.trackingwil') }}">
-                        <div class="small-box bg-primary">
-                            <div class="inner" style="background-color: #D988B9">
-                                <p>Purchase Request</p>
-                                <h3>Tracking Wilayah</h3>
+                @if (Auth::user()->role == 2 || Auth::user()->role == 3)
+                    <div class="col-lg-3 col-6">
+                        <a href="{{ route('product.trackingwil') }}">
+                            <div class="small-box bg-primary">
+                                <div class="inner" style="background-color: #D988B9">
+                                    <p>Purchase Request</p>
+                                    <h3>Tracking Wilayah</h3>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-route"></i>
+                                </div>
                             </div>
-                            <div class="icon">
-                                <i class="fas fa-route"></i>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
                 @endif
                 @if (Auth::user()->role == 0 || Auth::user()->role == 5)
-                <div class="col-lg-3 col-6">
-                    <a href="{{ route('product.approvedPO') }}"> 
-                        <div class="small-box bg-primary">
-                            <div class="inner" style="background-color: #A7D397">
-                                <p>APRROVAL</p>
-                                <h3>PO</h3>
+                    <div class="col-lg-3 col-6">
+                        <a href="{{ route('product.approvedPO') }}">
+                            <div class="small-box bg-primary">
+                                <div class="inner" style="background-color: #A7D397">
+                                    <p>APRROVAL</p>
+                                    <h3>PO</h3>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-clipboard-check"></i>
+                                </div>
                             </div>
-                            <div class="icon">
-                                <i class="fas fa-clipboard-check"></i>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <a href="{{ route('product.aprrovedPO_PL') }}">
+                            <div class="small-box bg-primary">
+                                <div class="inner" style="background-color: #186F65">
+                                    <p>APRROVAL</p>
+                                    <h3>PO/PL</h3>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-tasks"></i>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-6">
-                    <a href="{{ route('product.aprrovedPO_PL') }}"> 
-                        <div class="small-box bg-primary">
-                            <div class="inner" style="background-color: #186F65">
-                                <p>APRROVAL</p>
-                                <h3>PO/PL</h3>
+                        </a>
+                    </div>
+                @endif
+                @if (Auth::user()->role == 0 || Auth::user()->role == 6)
+                    <div class="col-lg-3 col-6">
+                        <a href="{{ route('product.justifikasi') }}">
+                            <div class="small-box bg-primary">
+                                <div class="inner" style="background-color: #afc324">
+                                    <p>Menu</p>
+                                    <h3>Justifikasi</h3>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-folder-open"></i>
+                                </div>
                             </div>
-                            <div class="icon">
-                                <i class="fas fa-tasks"></i>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
                 @endif
             </div>
         </div>
